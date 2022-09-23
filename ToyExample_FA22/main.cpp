c909d6a68c61e5bbb0be5a68e9e3769526ed6603
BuschMath <busch@iowacentral.edu>
2022-09-23 10:59:59 -0500

    Add project files.

---
+++ ToyExample_FA22/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char a, b, c;
+	cout << "Input 1: ";
+	cin >> a;
+	cout << endl << a;
+	cout << "\nInput 2: ";
+	cin.get(b);
+	cout << endl << b;
+	cout << "\nInput 3: ";
+	cin >> c;
+	cout << endl << c;
+
+	return 0;
+}
