a8c576b160189472c0dd1e49c9265cd8196c7f41
BuschMath <busch@iowacentral.edu>
2022-11-16 10:29:14 -0600

    CLimits

--- ToyExample_FA22/main.cpp
+++ ToyExample_FA22/main.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-void ExampleFunction();
-void Function();
-
 int main()
 {
-	for (int i = 0; i <= 10; i++)
-	{
-		ExampleFunction();
-		Function();
-	}
+	cout << "Size of:\nchar: " << sizeof(char) << "\n\nshort: " << sizeof(short) << "\n\nint: ";
+	cout << sizeof(int) << "\n\nlong: " << sizeof(long) << "\n\nbool: " << sizeof(bool) << "\n\n";
+
+	cout << "Min/Max:\nchar min/max: " << CHAR_MIN << " " << CHAR_MAX << "\n\nshort min/max: ";
+	cout << SHRT_MIN << " " << SHRT_MAX << "\n\nint min/max: " << INT_MIN << " " << INT_MAX;
+	cout << "\n\nlong min/max: " << LONG_MIN << " " << LONG_MAX << endl;
+
+	long long ex = pow(3000000000, 2);
+
+	cout << ex;
 
 	return 0;
-}
-
-void ExampleFunction()
-{
-	static int counter = 0;
-
-	counter += 3;
-	cout << "Count: " << counter << endl;
-}
-
-void Function()
-{
-	int counter = 0;
-
-	counter += 4;
-	cout << "A Count: " << counter << endl;
 }
